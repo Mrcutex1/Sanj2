9a6c394d6d0d6b3b74993a73343b198c1dd064e7
sparrow9616 <gamingleague09@gmail.com>
2024-11-11 09:18:25 +0530

    fixed some errors

--- AviaxMusic/plugins/tools/welcome.py
+++ AviaxMusic/plugins/tools/welcome.py
@@ -78,19 +78,19 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("DAXXMUSIC/assets/wel2.png")
+    background = Image.open("AviaxMusic/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((575, 575))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=70)
-    welcome_font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=61)
+    font = ImageFont.truetype('AviaxMusic/assets/font4.ttf', size=70)
+    welcome_font = ImageFont.truetype('AviaxMusic/assets/font4.ttf', size=61)
     #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
     #
- #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
+    # draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
     draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
-#    draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
-  #  draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
+    # draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
+    # draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
 
     #
     pfp_position = (48, 88)
@@ -145,19 +145,25 @@ async def greet_new_member(_, member: ChatMemberUpdated):
     if member.new_chat_member and not member.old_chat_member and member.new_chat_member.status != "kicked":
     
         try:
-            pic = await app.download_media(
-                user.photo.big_file_id, file_name=f"pp{user.id}.png"
-            )
+            async for photo in app.get_chat_photos(user.id,1): 
+                pic = await app.download_media(photo.file_id, file_name=f'{user.id}.jpg')
         except AttributeError:
-            pic = "DAXXMUSIC/assets/upic.png"
+            pic = "AviaxMusic/assets/upic.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
             except Exception as e:
                 LOGGER.error(e)
         try:
+            name = user.full_name
+            if not name:
+                name = ""
+            chatname = member.chat.title if member.chat.title else ""
+            id = user.id if user.id else ""
+            uname = user.username if user.username else ""
+
             welcomeimg = welcomepic(
-                pic, user.first_name, member.chat.title, user.id, user.username
+                pic, name, chatname, id, uname
             )
             button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
             add_button_text = "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏"
@@ -181,7 +187,8 @@ async def greet_new_member(_, member: ChatMemberUpdated):
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton(button_text, url=deep_link)],
                     [InlineKeyboardButton(text=add_button_text, url=add_link)],
-                ])
+                ]),
+                parse_mode=ParseMode.DEFAULT
             )
         except Exception as e:
             LOGGER.error(e)
